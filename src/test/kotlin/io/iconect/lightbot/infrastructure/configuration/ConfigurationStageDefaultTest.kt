3a0320600703b9fa5546a8d3e94d8dd0991cdf72
larmic <mail@larmic.de>
2017-11-24 15:05:48 +0100

    first commit

---
+++ src/test/kotlin/io/iconect/lightbot/infrastructure/configuration/ConfigurationStageDefaultTest.kt
@@ -0,0 +1,25 @@
+package io.iconect.lightbot.infrastructure.configuration
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ConfigurationStageDefaultTest {
+
+    @Autowired
+    lateinit var configuration: Configuration
+
+    @Test
+    fun `check configuration initializing on default stage`() {
+        Assertions.assertThat(configuration.bot?.identifier).isEqualTo("light-bot@iconect.io")
+        Assertions.assertThat(configuration.bot?.username).isEqualTo("light-bot")
+        Assertions.assertThat(configuration.bot?.password).isEqualTo("light-bot")
+        Assertions.assertThat(configuration.bot?.notificationUrl).isEqualTo("http://localhost:8085/api/notify")
+        Assertions.assertThat(configuration.server?.url).isEqualTo("http://localhost:8080/")
+    }
+}
---
+++ src/test/kotlin/io/iconect/lightbot/infrastructure/configuration/ConfigurationTest.kt
@@ -0,0 +1,27 @@
+package io.iconect.lightbot.infrastructure.configuration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@ActiveProfiles("unittest")
+class ConfigurationTest {
+
+    @Autowired
+    lateinit var configuration: Configuration
+
+    @Test
+    fun `check configuration initializing`() {
+        assertThat(configuration.bot?.identifier).isEqualTo("unit@test.bot")
+        assertThat(configuration.bot?.username).isEqualTo("unit-test-bot-username")
+        assertThat(configuration.bot?.password).isEqualTo("unit-test-bot-password")
+        assertThat(configuration.bot?.notificationUrl).isEqualTo("http://bot.unit.test/api/notify")
+        assertThat(configuration.server?.url).isEqualTo("http://server.unit.test/")
+    }
+}
