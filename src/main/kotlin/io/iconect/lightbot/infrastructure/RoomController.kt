c13b34f9066404c54119033272fc069f3e14f8f5
larmic <mail@larmic.de>
2017-12-10 13:31:52 +0100

    add a simple hateoas test

--- src/main/kotlin/io/iconect/lightbot/infrastructure/RoomController.kt
+++ src/main/kotlin/io/iconect/lightbot/infrastructure/RoomController.kt
@@ -1,8 +1,11 @@
 package io.iconect.lightbot.infrastructure
 
+import io.iconect.lightbot.domain.Room
 import io.iconect.lightbot.domain.RoomRepository
 import io.iconect.lightbot.infrastructure.model.RoomDto
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -17,9 +20,15 @@ class RoomController {
     @RequestMapping(value = "/api/rooms", method = arrayOf(RequestMethod.GET), produces = arrayOf("application/json"))
     fun getAllRooms(): ResponseEntity<List<RoomDto>> {
         val rooms = roomRepository.loadAll()
-                .map { r -> RoomDto(r.identifier, r.designation) }
+                .map { r -> mapToRoomDto(r) }
 
         return ResponseEntity.ok<List<RoomDto>>(rooms)
+    }
+
+    private fun mapToRoomDto(room: Room): RoomDto {
+        val roomDto = RoomDto(room.identifier, room.designation)
+        roomDto.add(linkTo(methodOn(RoomController::class.java).getAllRooms()).withSelfRel())
+        return roomDto
     }
 
 }
--- src/main/kotlin/io/iconect/lightbot/infrastructure/model/RoomDto.kt
+++ src/main/kotlin/io/iconect/lightbot/infrastructure/model/RoomDto.kt
@@ -1,5 +1,6 @@
 package io.iconect.lightbot.infrastructure.model
 
+import org.springframework.hateoas.ResourceSupport
 import java.io.Serializable
 
-data class RoomDto constructor(var identifier: String? = null, var designation: String? = null) : Serializable
+data class RoomDto constructor(var identifier: String? = null, var designation: String? = null) : Serializable, ResourceSupport()
