de548b7d05be0fb578a9b5dfbe119994b01eee7e
larmic <mail@larmic.de>
2017-12-20 18:20:15 +0100

    add value to CharacteristicDto.kt

--- src/main/kotlin/io/iconect/lightbot/infrastructure/model/CharacteristicDto.kt
+++ src/main/kotlin/io/iconect/lightbot/infrastructure/model/CharacteristicDto.kt
@@ -2,11 +2,11 @@ package io.iconect.lightbot.infrastructure.model
 
 import io.iconect.lightbot.domain.service.characteristic.Characteristic
 
-data class CharacteristicDto(val iid: Int, val type: String) {
+data class CharacteristicDto(val iid: Int, val type: String, val value: String?) {
 
     companion object {
         fun from(characteristic: Characteristic): CharacteristicDto {
-            return CharacteristicDto(characteristic.instanceId, UuidToTypeMapper.map(characteristic.uuid))
+            return CharacteristicDto(characteristic.instanceId, UuidToTypeMapper.map(characteristic.uuid), characteristic.value)
         }
     }
 
--- src/test/kotlin/io/iconect/lightbot/infrastructure/model/CharacteristicDtoTest.kt
+++ src/test/kotlin/io/iconect/lightbot/infrastructure/model/CharacteristicDtoTest.kt
@@ -9,11 +9,13 @@ class CharacteristicDtoTest {
     @Test
     fun `map domain model to dto`() {
         val name = Name(37)
+        name.updateName("unit-test-value-name")
 
         val nameDto = CharacteristicDto.from(name)
 
         Assertions.assertThat(nameDto.iid).isEqualTo(37)
         Assertions.assertThat(nameDto.type).isEqualTo("23")
+        Assertions.assertThat(nameDto.value).isEqualTo("unit-test-value-name")
     }
 
 }
