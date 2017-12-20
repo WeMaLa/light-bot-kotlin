55e7205912fb31d5843662593b9410bb920c90d7
larmic <mail@larmic.de>
2017-12-20 19:06:41 +0100

    remove deprecated domain model incl. application and infrastructure

--- src/main/kotlin/io/iconect/lightbot/infrastructure/model/RoomDto.kt
+++
@@ -1,6 +0,0 @@
-package io.iconect.lightbot.infrastructure.model
-
-import org.springframework.hateoas.ResourceSupport
-import java.io.Serializable
-
-data class RoomDto constructor(var identifier: String? = null, var designation: String? = null) : Serializable, ResourceSupport()
