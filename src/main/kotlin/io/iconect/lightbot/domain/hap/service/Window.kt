366b06df05c4bef262fd2bf1d029aebd0760c95f
larmic <mail@larmic.de>
2018-01-12 14:38:40 +0100

    #9 add services for window service

--- src/main/kotlin/io/iconect/lightbot/domain/hap/service/Window.kt
+++ src/main/kotlin/io/iconect/lightbot/domain/hap/service/Window.kt
@@ -1,13 +1,21 @@
 package io.iconect.lightbot.domain.hap.service
 
-import io.iconect.lightbot.domain.hap.service.characteristic.Characteristic
+import io.iconect.lightbot.domain.hap.service.characteristic.CurrentPosition
+import io.iconect.lightbot.domain.hap.service.characteristic.Name
+import io.iconect.lightbot.domain.hap.service.characteristic.TargetPosition
 
 // page 228
-class Window(override val instanceId: Int) : Service {
+class Window(override val instanceId: Int,
+             private val targetPositionInstanceId: Int,
+             private val currentPositionInstanceId: Int,
+             private val nameInstanceId: Int) : Service {
 
     override val uuid = "0000008B-0000-1000-8000-0026BB765291"
     override val type = "public.hap.service.window"
-    override val characteristics = emptyList<Characteristic>()
+    override val characteristics = listOf(
+            TargetPosition(targetPositionInstanceId),
+            CurrentPosition(currentPositionInstanceId),
+            Name(nameInstanceId))
     override val hidden = false
     override val primaryService = true
     override val linkedServices = emptyList<Int>()
