b9164eae63a298487bc00d500a581d51f5c47c13
larmic <mail@larmic.de>
2018-01-13 22:45:03 +0100

    bind event repository to AccessoryFactory.kt

--- src/main/kotlin/io/iconect/lightbot/domain/hap/AccessoryFactory.kt
+++ src/main/kotlin/io/iconect/lightbot/domain/hap/AccessoryFactory.kt
@@ -4,17 +4,19 @@ import io.iconect.lightbot.domain.hap.service.LightBulb
 import io.iconect.lightbot.domain.hap.service.Thermostat
 import io.iconect.lightbot.domain.hap.service.Window
 import io.iconect.lightbot.domain.hap.service.characteristic.Name
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class AccessoryFactory {
+class AccessoryFactory @Autowired constructor(private val characteristicAdjustedEventRepository: CharacteristicAdjustedEventRepository) {
 
     fun createLightBulbAccessory(accessoryInstanceId: Int,
                                  instanceId: Int,
                                  onInstanceId: Int,
                                  nameInstanceId: Int,
                                  name: String): Accessory {
-        val lightBulb = LightBulb(instanceId, accessoryInstanceId, onInstanceId, nameInstanceId, { _, _, _ -> })
+        val eventPublisher: (Int, Int, String) -> Unit = { aid, iid, value -> characteristicAdjustedEventRepository.pushEvent(CharacteristicAdjustedEvent(aid, iid, value)) }
+        val lightBulb = LightBulb(instanceId, accessoryInstanceId, onInstanceId, nameInstanceId, eventPublisher)
         (lightBulb.characteristics.first { c -> c is Name } as Name).updateName(name)
         return Accessory(accessoryInstanceId, listOf(lightBulb))
     }
@@ -25,7 +27,8 @@ class AccessoryFactory {
                                   currentTemperatureInstanceId: Int,
                                   nameInstanceId: Int,
                                   name: String): Accessory {
-        val thermostat = Thermostat(instanceId, accessoryInstanceId, targetTemperatureInstanceId, currentTemperatureInstanceId, nameInstanceId, { _, _, _ -> })
+        val eventPublisher: (Int, Int, String) -> Unit = { aid, iid, value -> characteristicAdjustedEventRepository.pushEvent(CharacteristicAdjustedEvent(aid, iid, value)) }
+        val thermostat = Thermostat(instanceId, accessoryInstanceId, targetTemperatureInstanceId, currentTemperatureInstanceId, nameInstanceId, eventPublisher)
         (thermostat.characteristics.first { c -> c is Name } as Name).updateName(name)
         return Accessory(accessoryInstanceId, listOf(thermostat))
     }
@@ -36,7 +39,8 @@ class AccessoryFactory {
                               currentPositionInstanceId: Int,
                               nameInstanceId: Int,
                               name: String): Accessory {
-        val window = Window(instanceId, accessoryInstanceId, targetPositionInstanceId, currentPositionInstanceId, nameInstanceId, { _, _, _ -> })
+        val eventPublisher: (Int, Int, String) -> Unit = { aid, iid, value -> characteristicAdjustedEventRepository.pushEvent(CharacteristicAdjustedEvent(aid, iid, value)) }
+        val window = Window(instanceId, accessoryInstanceId, targetPositionInstanceId, currentPositionInstanceId, nameInstanceId, eventPublisher)
         (window.characteristics.first { c -> c is Name } as Name).updateName(name)
         return Accessory(accessoryInstanceId, listOf(window))
     }
--- src/test/kotlin/io/iconect/lightbot/domain/hap/AccessoryFactoryTest.kt
+++ src/test/kotlin/io/iconect/lightbot/domain/hap/AccessoryFactoryTest.kt
@@ -1,12 +1,14 @@
 package io.iconect.lightbot.domain.hap
 
 import io.iconect.lightbot.domain.hap.service.characteristic.*
+import io.iconect.lightbot.infrastructure.CachedCharacteristicAdjustedEventRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class AccessoryFactoryTest {
 
-    private val accessoryFactory: AccessoryFactory = AccessoryFactory()
+    private val eventRepository = CachedCharacteristicAdjustedEventRepository()
+    private val accessoryFactory: AccessoryFactory = AccessoryFactory(eventRepository)
 
     @Test
     fun `create light bulb accessory`() {
@@ -25,11 +27,24 @@ class AccessoryFactoryTest {
 
         val name = accessory.services[0].characteristics.first { c -> c is Name } as Name
         assertThat(name.instanceId).isEqualTo(4)
+
+        var popNextEvent = eventRepository.popNextEvent()
+        assertThat(popNextEvent?.accessoryId).isEqualTo(1)
+        assertThat(popNextEvent?.characteristicId).isEqualTo(4)
+        assertThat(popNextEvent?.value).isEqualTo("unit-test-light-bulb")
+
+        on.adjustValue("on")
+        popNextEvent = eventRepository.popNextEvent()
+        assertThat(popNextEvent?.accessoryId).isEqualTo(1)
+        assertThat(popNextEvent?.characteristicId).isEqualTo(3)
+        assertThat(popNextEvent?.value).isEqualTo("on")
+
+        assertThat(eventRepository.popNextEvent()).isNull()
     }
 
     @Test
     fun `create thermostat accessory`() {
-        val accessory = accessoryFactory.createThermostatAccessory(1, 2, 3, 4, 5, "unit-test-light-bulb")
+        val accessory = accessoryFactory.createThermostatAccessory(1, 2, 3, 4, 5, "unit-test-thermostat")
 
         assertThat(accessory.instanceId).isEqualTo(1)
         assertThat(accessory.services.size).isEqualTo(1)
@@ -48,11 +63,24 @@ class AccessoryFactoryTest {
 
         val name = accessory.services[0].characteristics.first { c -> c is Name } as Name
         assertThat(name.instanceId).isEqualTo(5)
+
+        var popNextEvent = eventRepository.popNextEvent()
+        assertThat(popNextEvent?.accessoryId).isEqualTo(1)
+        assertThat(popNextEvent?.characteristicId).isEqualTo(5)
+        assertThat(popNextEvent?.value).isEqualTo("unit-test-thermostat")
+
+        targetTemperature.adjustValue("19.8")
+        popNextEvent = eventRepository.popNextEvent()
+        assertThat(popNextEvent?.accessoryId).isEqualTo(1)
+        assertThat(popNextEvent?.characteristicId).isEqualTo(3)
+        assertThat(popNextEvent?.value).isEqualTo("19.8")
+
+        assertThat(eventRepository.popNextEvent()).isNull()
     }
 
     @Test
     fun `create window accessory`() {
-        val accessory = accessoryFactory.createWindowAccessory(1, 2, 3, 4, 5, "unit-test-light-bulb")
+        val accessory = accessoryFactory.createWindowAccessory(1, 2, 3, 4, 5, "unit-test-window")
 
         assertThat(accessory.instanceId).isEqualTo(1)
         assertThat(accessory.services.size).isEqualTo(1)
@@ -63,13 +91,26 @@ class AccessoryFactoryTest {
         assertThat(accessory.services[0].characteristics.filter { c -> c is CurrentPosition }.size).isEqualTo(1)
         assertThat(accessory.services[0].characteristics.filter { c -> c is Name }.size).isEqualTo(1)
 
-        val targetTemperature = accessory.services[0].characteristics.first { c -> c is TargetPosition } as TargetPosition
-        assertThat(targetTemperature.instanceId).isEqualTo(3)
+        val targetPosition = accessory.services[0].characteristics.first { c -> c is TargetPosition } as TargetPosition
+        assertThat(targetPosition.instanceId).isEqualTo(3)
 
-        val currentTemperature = accessory.services[0].characteristics.first { c -> c is CurrentPosition } as CurrentPosition
-        assertThat(currentTemperature.instanceId).isEqualTo(4)
+        val currentPosition = accessory.services[0].characteristics.first { c -> c is CurrentPosition } as CurrentPosition
+        assertThat(currentPosition.instanceId).isEqualTo(4)
 
         val name = accessory.services[0].characteristics.first { c -> c is Name } as Name
         assertThat(name.instanceId).isEqualTo(5)
+
+        var popNextEvent = eventRepository.popNextEvent()
+        assertThat(popNextEvent?.accessoryId).isEqualTo(1)
+        assertThat(popNextEvent?.characteristicId).isEqualTo(5)
+        assertThat(popNextEvent?.value).isEqualTo("unit-test-window")
+
+        targetPosition.adjustValue("19")
+        popNextEvent = eventRepository.popNextEvent()
+        assertThat(popNextEvent?.accessoryId).isEqualTo(1)
+        assertThat(popNextEvent?.characteristicId).isEqualTo(3)
+        assertThat(popNextEvent?.value).isEqualTo("19")
+
+        assertThat(eventRepository.popNextEvent()).isNull()
     }
 }
