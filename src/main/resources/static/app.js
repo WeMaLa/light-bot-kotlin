a867c5336616a5128ce3b4f7ee014c948894ab09
larmic <mail@larmic.de>
2018-01-14 15:42:04 +0100

    show events in ui

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,26 +13,46 @@ function onReady() {
         .catch(function (ex) {
             console.log('parsing failed', ex)
         });
-    connectWebSocket();
+    connectWebSocket('/topic/status', updateMessageStatus);
+    connectWebSocket('/topic/event', eventReceived);
 }
 
 function updateMessageStatus(status) {
     document.getElementById('vHabStatus').innerHTML = status;
 }
 
-function connectWebSocket() {
+function eventReceived(json) {
+    var event = JSON.parse(json);
+    var accessoryIdAttribute = document.createAttribute('data-accessory-id');
+    accessoryIdAttribute.value = event.accessoryId;
+    var characteristicIdAttribute = document.createAttribute('data-characteristic-id');
+    characteristicIdAttribute.value = event.characteristicId;
+    var valueAttribute = document.createAttribute('data-value');
+    valueAttribute.value = event.value;
+
+    var textnode = document.createTextNode(json);
+
+    var div = document.createElement("div");
+    div.setAttributeNode(accessoryIdAttribute);
+    div.setAttributeNode(characteristicIdAttribute);
+    div.setAttributeNode(valueAttribute);
+    div.appendChild(textnode);
+    document.getElementById('events').appendChild(div);
+}
+
+function connectWebSocket(topic, callback) {
     var socket = new SockJS('/vhab-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('STOMP: connected to ' + frame);
-        stompClient.subscribe('/topic/status', function (status) {
-            console.log('STOMP: receive status ' + status.body);
-            updateMessageStatus(status.body);
+        stompClient.subscribe(topic, function (status) {
+            console.log('STOMP: receive content ' + status.body + ' on topic ' + topic);
+            callback(status.body);
         });
     }, function (error) {
         console.log('STOMP: ' + error);
-        setTimeout(connectWebSocket, 10000);
-        console.log('STOMP: Reconecting in 10 seconds');
+        setTimeout(connectWebSocket(topic, callback), 10000);
+        console.log('STOMP: Reconnecting in 10 seconds');
     });
 }
 
