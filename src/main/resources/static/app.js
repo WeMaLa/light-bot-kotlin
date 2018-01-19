352ce3c2d13476bc455db4b35da9159f63f43945
larmic <mail@larmic.de>
2018-01-19 21:42:09 +0100

    update kotlin to 1.2.20

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -51,19 +51,68 @@ function connectWebSocket(topic, callback) {
 
 function initServiceOverlays() {
     document.getElementById('services').appendChild(createWindow('kitchen-window', 11000, 11100, 11101, 11102, 11103));
+    updateWindowState('kitchen-window', 11000, 11100, 11101, 11102, 11103);
+    document.getElementById('services').appendChild(createLightBulb('kitchen-light-bulb', 12000, 12100, 12101, 12102));
+    updateLightBulbState('kitchen-light-bulb', 12000, 12100, 12101, 12102);
 }
 
-function createWindow(id, accessoryId, serviceId, targetPositionCharacteristicId, currentPositionCharacteristicId, nameCharacteristicId) {
+function createWindow(htmlId, accessoryId, serviceId, targetPositionCharacteristicId, currentPositionCharacteristicId, nameCharacteristicId) {
     var div = document.createElement("div");
-    div.setAttribute("id", id)
+    div.setAttribute("id", htmlId);
     div.setAttribute('data-type', 'window');
     div.setAttribute('data-accessory-id', accessoryId);
     div.setAttribute('data-service-id', serviceId);
     div.setAttribute('data-current-position-characteristic-id', currentPositionCharacteristicId);
     div.setAttribute('data-target-position-characteristic-id', targetPositionCharacteristicId);
     div.setAttribute('data-name-characteristic-id', nameCharacteristicId);
-    div.appendChild(document.createTextNode(id));
+    div.setAttribute('class', 'vhab-window-state');
+    div.setAttribute('style', 'display:none');
+    div.appendChild(document.createTextNode(htmlId));
     return div;
+}
+
+function updateWindowState(htmlId, accessoryId, serviceId, targetPositionCharacteristicId, currentPositionCharacteristicId, nameCharacteristicId) {
+    var targetPositionCharacteristic = fetchCharacteristic(accessoryId, serviceId, targetPositionCharacteristicId);
+    console.log(targetPositionCharacteristic);
+    var currentPositionCharacteristic = fetchCharacteristic(accessoryId, serviceId, currentPositionCharacteristicId);
+    console.log(currentPositionCharacteristic);
+    var nameCharacteristic = fetchCharacteristic(accessoryId, serviceId, nameCharacteristicId);
+    console.log(nameCharacteristic);
+}
+
+function createLightBulb(htmlId, accessoryId, serviceId, onCharacteristicId, nameCharacteristicId) {
+    var div = document.createElement("div");
+    div.setAttribute("id", htmlId);
+    div.setAttribute('data-type', 'lightBulb');
+    div.setAttribute('data-accessory-id', accessoryId);
+    div.setAttribute('data-service-id', serviceId);
+    div.setAttribute('data-on-characteristic-id', onCharacteristicId);
+    div.setAttribute('data-name-characteristic-id', nameCharacteristicId);
+    div.setAttribute('class', 'vhab-window-state');
+    div.setAttribute('style', 'display:none');
+    div.appendChild(document.createTextNode(htmlId));
+    return div;
+}
+
+function updateLightBulbState(htmlId, accessoryId, serviceId, onCharacteristicId, nameCharacteristicId) {
+    var onCharacteristic = fetchCharacteristic(accessoryId, serviceId, onCharacteristicId);
+    console.log(onCharacteristic);
+    var nameCharacteristic = fetchCharacteristic(accessoryId, serviceId, nameCharacteristicId);
+    console.log(nameCharacteristic);
+}
+
+function fetchCharacteristic(accessoryId, serviceId, characteristicId) {
+    fetch('/api/accessories/' + accessoryId + '/services/' + serviceId + '/characteristics/' + characteristicId)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log('parsed characteristic json', json);
+            return json;
+        })
+        .catch(function (ex) {
+            console.log('parsing failed', ex)
+        });
 }
 
 if (document.readyState === 'complete' || document.readyState !== 'loading') {
