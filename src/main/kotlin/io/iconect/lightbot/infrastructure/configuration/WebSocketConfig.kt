6665f2fd5870a3c50b44f76d9007e13339372d53
larmic <mail@larmic.de>
2018-03-01 09:49:12 +0100

    upgrade spring boot to 2.0.0.RELEASE

--- src/main/kotlin/io/iconect/lightbot/infrastructure/configuration/WebSocketConfig.kt
+++ src/main/kotlin/io/iconect/lightbot/infrastructure/configuration/WebSocketConfig.kt
@@ -2,13 +2,13 @@ package io.iconect.lightbot.infrastructure.configuration
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig : AbstractWebSocketMessageBrokerConfigurer() {
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
         config.enableSimpleBroker("/topic")
